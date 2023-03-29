46a02aa037766d1172d933e43c3c44dd245317aa
aggenov <a.g.genov@gmail.com>
2023-03-29 10:02:17 +0300

    completed HW

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryLists = document.querySelector(".gallery");
+let markap = "";
+galleryItems.forEach((item) => {
+  const { preview, original, description } = item;
+  const buildMarkup = (item) =>
+    `<li class ='gallery__item'>
+            <a class ='gallery__link' href="${original}">
+                <img class ='gallery__image'
+                src='${preview}'
+                data-source='${original}'
+                alt='${description}'/>
+            </a>
+        </li>`;
+  markap += buildMarkup(item);
+});
+galleryLists.insertAdjacentHTML("afterbegin", markap);
+
+galleryLists.addEventListener("click", onOpenImage);
+
+function onOpenImage(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  let instance = basicLightbox.create(
+    `
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscapePress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscapePress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscapePress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryLists = document.querySelector(".gallery");
+
+const markap = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class ='gallery__item'>
+            <a class ='gallery__link' href="${original}">
+                <img class ='gallery__image'
+                src='${preview}'
+                alt='${description}'/>
+            </a>
+        </li>`;
+  })
+  .join("");
+
+galleryLists.insertAdjacentHTML("afterbegin", markap);
+
+new SimpleLightbox(".gallery a", {});
